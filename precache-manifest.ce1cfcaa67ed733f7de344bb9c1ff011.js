a8a91f87175fb122e05d5d66b2165fa58c0b3cf2
Emily Sanchez <emilysanchez@Emilys-MacBook-Pro.local>
2019-04-04 10:22:33 -0700

    Updates

---
+++ precache-manifest.ce1cfcaa67ed733f7de344bb9c1ff011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fefdb72719857203711",
+    "url": "/farmers-app/static/js/runtime~main.bdad43ed.js"
+  },
+  {
+    "revision": "06dc2e5012d2ef303e95",
+    "url": "/farmers-app/static/js/main.64db279b.chunk.js"
+  },
+  {
+    "revision": "721b46dca57e640a1157",
+    "url": "/farmers-app/static/js/2.deb73580.chunk.js"
+  },
+  {
+    "revision": "06dc2e5012d2ef303e95",
+    "url": "/farmers-app/static/css/main.36c41d16.chunk.css"
+  },
+  {
+    "revision": "721b46dca57e640a1157",
+    "url": "/farmers-app/static/css/2.f7da3c95.chunk.css"
+  },
+  {
+    "revision": "4c15325d2eafdab6f6be827b2f3aec80",
+    "url": "/farmers-app/index.html"
+  }
+];
