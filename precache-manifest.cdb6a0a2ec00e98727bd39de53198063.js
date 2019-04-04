f0e4fc6da09fefe7eec182bcbde91f2b9b102683
Emily Sanchez <emilysanchez@Emilys-MacBook-Pro.local>
2019-04-04 10:47:28 -0700

    Updates

---
+++ precache-manifest.cdb6a0a2ec00e98727bd39de53198063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "acbf2456013cfed68920",
+    "url": "/FarmersProject/static/js/runtime~main.61aae7c8.js"
+  },
+  {
+    "revision": "5fb881053508e8788db7",
+    "url": "/FarmersProject/static/js/main.0f59f781.chunk.js"
+  },
+  {
+    "revision": "721b46dca57e640a1157",
+    "url": "/FarmersProject/static/js/2.deb73580.chunk.js"
+  },
+  {
+    "revision": "5fb881053508e8788db7",
+    "url": "/FarmersProject/static/css/main.36c41d16.chunk.css"
+  },
+  {
+    "revision": "721b46dca57e640a1157",
+    "url": "/FarmersProject/static/css/2.f7da3c95.chunk.css"
+  },
+  {
+    "revision": "75edc0ec91cd69be64474903fd6a7931",
+    "url": "/FarmersProject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/farmers-app/precache-manifest.ce1cfcaa67ed733f7de344bb9c1ff011.js"
+  "/FarmersProject/precache-manifest.cdb6a0a2ec00e98727bd39de53198063.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/farmers-app/index.html", {
+workbox.routing.registerNavigationRoute("/FarmersProject/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
